ee0d0734a16dda896a55528a8b0701ec26a94c59
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-01-19 16:07:05 +0100

    feat(search): first attempt at an search interface
    This adds:
    - Basic component structure
    - Api to define filters and to display result

--- addon/controllers/project/link-existing.js
+++ addon/controllers/project/link-existing.js
@@ -1,14 +1,15 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
-import { task } from "ember-concurrency-decorators";
+import { task, lastValue } from "ember-concurrency-decorators";
 import { action } from "@ember/object";
 
 export default class ProjectLinkExistingController extends Controller {
   @service constructionProject;
 
+  @lastValue("search") searchResults;
   @task
   *search(query) {
-    yield this.constructionProject.search(query);
+    return yield this.constructionProject.search(query);
   }
 
   @action
