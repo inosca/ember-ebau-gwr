319ee489d75f3dff7129ae97feb4ee0adaf36fee
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-01-22 21:58:05 +0100

    feat(search-project): add validation for filter form

--- addon/controllers/search-project.js
+++ addon/controllers/search-project.js
@@ -7,14 +7,60 @@ import { tracked } from "@glimmer/tracking";
 
 export default class SearchProjectController extends Controller {
   @service constructionProject;
+  @service intl;
 
   @tracked extendedSearch = false;
+  @tracked validationErrors = {};
 
   projectStatusOptions = projectStatusOptions;
+
+  // This is ugly state modifying code with side effects. But i didnt know a nicer way.
+  validateSearchQuery({ realestateIdentification, modifyDate, createDate }) {
+    this.validationErrors = {};
+    // Since the api needs a number for the realestateIdentification
+    // We have to check that the number is set.
+    if (
+      (realestateIdentification.EGRID ||
+        realestateIdentification.numberSuffix ||
+        realestateIdentification.subDistrict ||
+        realestateIdentification.lot) &&
+      !realestateIdentification.number
+    ) {
+      this.validationErrors.realestateIdentification = this.intl.t(
+        "ember-gwr.searchProject.validation.realestateIdentification"
+      );
+    }
+
+    // If setting a date filter, both the to and from date have to be set.
+    if (
+      (modifyDate.dateTo || modifyDate.dateFrom) &&
+      !(modifyDate.dateTo && modifyDate.dateFrom)
+    ) {
+      this.validationErrors.modifyDate = this.intl.t(
+        "ember-gwr.searchProject.validation.bothDatesSet"
+      );
+    }
+
+    if (
+      (createDate.dateTo || createDate.dateFrom) &&
+      !(createDate.dateTo && createDate.dateFrom)
+    ) {
+      this.validationErrors.createDate = this.intl.t(
+        "ember-gwr.searchProject.validation.bothDatesSet"
+      );
+    }
+    // Return a boolean if there are errors or not
+    return Object.keys(this.validationErrors).length;
+  }
 
   @lastValue("search") searchResults;
   @task
   *search(query = {}) {
+    // There is only validation needed if the extended filters are used.
+    if (this.extendedSearch && this.validateSearchQuery(query)) {
+      return;
+    }
+
     query.constructionSurveyDept = 134200;
     return yield this.constructionProject.search(query);
   }
