f0737e4855d9e0acf1e96bf5d3abb645c669c0b1
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-03-03 12:05:42 +0100

    fix(adapter): remove adapter since a adapter should come from the hostap
    Also fix small issue where the model hook had a bug.

--- addon/routes/project/form.js
+++ addon/routes/project/form.js
@@ -7,7 +7,7 @@ export default class ProjectFormRoute extends Route {
   async model({ project_id: projectId }) {
     return {
       projectId,
-      instanceId: this.modelFor("application"),
+      instanceId: this.modelFor("application").id,
     };
   }
 }
