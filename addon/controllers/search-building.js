781ecd7ae193f9b5e2008f50b9daf4284472accf
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-03-22 10:06:13 +0100

    style(eslint): run eslint fix

--- addon/controllers/search-building.js
+++ addon/controllers/search-building.js
@@ -1,8 +1,8 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { task, lastValue } from "ember-concurrency-decorators";
 import { tracked } from "@glimmer/tracking";
+import { task, lastValue } from "ember-concurrency-decorators";
 import {
   languageOptions,
   periodOfConstructionOptions,
