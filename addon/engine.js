783e9313b272a6be0974b8207242ff89cdfa7257
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-04-21 17:28:40 +0200

    BREAKING CHANGE: move auth token and camac group to gwr config service

--- addon/engine.js
+++ addon/engine.js
@@ -17,7 +17,6 @@ export default class EmberGWREngine extends Engine {
       "dataImport",
       "store",
       "router",
-      "session",
     ],
   };
 }
--- tests/dummy/app/services/gwr-config.js
+++ tests/dummy/app/services/gwr-config.js
@@ -6,4 +6,12 @@ export default class GwrConfigService extends Service {
   cantonAbbreviation = "SZ";
   constructionSurveyDeptNumber = 134200;
   gwrAPI = "http://localhost:8010/proxy/regbl/api/ech0216/2";
+
+  get authToken() {
+    return "token";
+  }
+
+  get camacGroup() {
+    return "1";
+  }
 }
