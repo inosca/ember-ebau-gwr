ccb13daa445e558354e8e080d7a6574df3f19e3f
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-05-05 10:28:52 +0200

    ci(tests): use node v12

--- addon/components/login-modal.js
+++ addon/components/login-modal.js
@@ -2,7 +2,7 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import config from "ember-ebau-gwr/config/environment";
+import env from "ember-ebau-gwr/config/environment";
 
 export default class LoginModalComponent extends Component {
   @service authFetch;
@@ -13,7 +13,7 @@ export default class LoginModalComponent extends Component {
   @tracked municipality;
 
   get municipalities() {
-    return config.municipalities[this.config.cantonAbbreviation];
+    return env.municipalities[this.config.cantonAbbreviation];
   }
 
   @action
