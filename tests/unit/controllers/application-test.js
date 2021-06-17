f47f1cd6cdb35593f789ea511b19c2727433f271
Alana Luyten <alana.luyten@adfinis.com>
2021-06-17 11:10:37 +0200

    fix(login): hide login modal after authentication
    Make login modal invisible after successful login to ensure
    that the underlying ui is interactable.

---
+++ tests/unit/controllers/application-test.js
@@ -0,0 +1,15 @@
+import engineResolverFor from "ember-engines/test-support/engine-resolver-for";
+import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
+
+const modulePrefix = "ember-ebau-gwr";
+const resolver = engineResolverFor(modulePrefix);
+module("Unit | Controller | application", function (hooks) {
+  setupTest(hooks, { resolver, integration: true });
+
+  // TODO: Replace this with your real tests.
+  test("it exists", function (assert) {
+    const controller = this.owner.lookup("controller:application");
+    assert.ok(controller);
+  });
+});
