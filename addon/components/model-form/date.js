ee3b1b9b61c81d36d0743091e0802396e556dca8
Alana Luyten <alana.luyten@adfinis.com>
2021-09-16 10:51:56 +0200

    fix(model-form): ensure date values are reset on model update

--- addon/components/model-form/date.js
+++ addon/components/model-form/date.js
@@ -1,13 +1,13 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 
 // TODO: remove file once two-way binding issue of value is resolved in ember-pikaday
 // https://github.com/adopted-ember-addons/ember-pikaday/issues/212
 export default class ModelFormDateComponent extends Component {
-  @tracked value;
-
-  constructor(...args) {
-    super(...args);
-    this.value = this.args.value;
+  get value() {
+    return this.args.value;
   }
+
+  // prevent pikaday two-way binding from
+  // updating "value" field on input
+  set value(value) {}
 }
