f236fb67e1be78d2d32ae466a007895dd4f98e25
Jonas Cosandey <jonas.cosandey@adfinis.com>
2022-07-19 16:20:25 +0200

    feat(quarterly-closure): add link to housing stat for maintenance cost

--- addon/components/quarterly-closure/error-list/survey-department-errors.js
+++ addon/components/quarterly-closure/error-list/survey-department-errors.js
@@ -2,11 +2,16 @@ import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { trackedFunction } from "ember-resources/util/function";
+import { DateTime } from "luxon";
 
 export default class SurveyDepartmentErrors extends Component {
   @service quarterlyClosure;
   @service notification;
   @service intl;
+
+  get isFirstQuarter() {
+    return DateTime.now().toFormat("q") === "1";
+  }
 
   @tracked errors = trackedFunction(this, async () => {
     try {
