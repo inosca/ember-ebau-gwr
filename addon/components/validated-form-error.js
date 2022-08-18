c1535d3e7f97904b927eee303e341f4169bcb460
Stephan Hug <stephan.hug@adfinis.com>
2022-08-18 15:31:47 +0200

    feat: custom translated validate form error component

---
+++ addon/components/validated-form-error.js
@@ -0,0 +1,14 @@
+import { inject as service } from "@ember/service";
+import Component from "@glimmer/component";
+
+export default class ValidatedFormErrorComponent extends Component {
+  @service intl;
+
+  get errorString() {
+    return this.args.errors
+      ?.map((error) =>
+        this.intl.t(`ember-gwr.validation-errors.${error.type}`, error.context)
+      )
+      .join(", ");
+  }
+}
