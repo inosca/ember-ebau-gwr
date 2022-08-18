fe6f4b8bc13aa83e2bc1ccb6c96786fb91f21de6
Stephan Hug <stephan.hug@adfinis.com>
2022-08-18 15:33:52 +0200

    feat: building search validations

--- addon/validations/building-search.js
+++ addon/validations/building-search.js
@@ -1,3 +1,38 @@
+import { validatePresence } from "ember-changeset-validations/validators";
+
 export default {
-  // Not sure what kind of validations we need here
+  buildingEntranceNo: validatePresence({
+    presence: true,
+    on: ["EGID", "EGRID", "nameLong"],
+  }),
+  EGID: validatePresence(true),
+  realestateIdentification: {
+    number: validatePresence({
+      presence: true,
+      on: [
+        "realestateIdentification.EGRID",
+        "realestateIdentification.subDistrict",
+      ],
+    }),
+  },
+  createDate: {
+    dateTo: validatePresence({
+      presence: true,
+      on: ["createDate.dateFrom"],
+    }),
+    dateFrom: validatePresence({
+      presence: true,
+      on: ["createDate.dateTo"],
+    }),
+  },
+  modifyDate: {
+    dateTo: validatePresence({
+      presence: true,
+      on: ["modifyDate.dateFrom"],
+    }),
+    dateFrom: validatePresence({
+      presence: true,
+      on: ["modifyDate.dateFrom"],
+    }),
+  },
 };
