a81d03e9b25bc0b6ccafa6e1d2ac1f56b2229e69
Stephan Hug <stephan.hug@adfinis.com>
2022-08-18 15:32:52 +0200

    feat: filter component refactored to not use project form anymore

--- addon/components/search/filters.js
+++ addon/components/search/filters.js
@@ -2,20 +2,56 @@ import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { Changeset } from "ember-changeset";
+import lookupValidator from "ember-changeset-validations";
+import { task } from "ember-concurrency";
 
 export default class SearchFiltersComponent extends Component {
   @service intl;
 
   @tracked extendedSearch = false;
-  @tracked validationErrors = {};
+  @tracked errorOptions = [
+    {
+      value: true,
+      label: this.intl.t("ember-gwr.search.hasErrorOptions.true"),
+    },
+    {
+      value: false,
+      label: this.intl.t("ember-gwr.search.hasErrorOptions.false"),
+    },
+  ];
+
+  @tracked changeset;
 
   constructor(owner, args) {
     super(owner, args);
-    this.query = args.baseQuery ?? {
-      realestateIdentification: {},
-      createDate: {},
-      modifyDate: {},
-    };
+    this.changeset = new Changeset(
+      {
+        realestateIdentification: {
+          EGRID: null,
+          subDistrict: null,
+        },
+        createDate: {
+          dateFrom: null,
+          dateTo: null,
+        },
+        modifyDate: {
+          dateFrom: null,
+          dateTo: null,
+        },
+        ...this.args?.model,
+      },
+      lookupValidator(this.args.validations),
+      this.args.validations
+    );
+  }
+
+  get isSubmitBtnDisabled() {
+    return !this.hasChanges;
+  }
+
+  get hasChanges() {
+    return !!this.changeset.changes.find(({ value }) => value !== "");
   }
 
   @action
@@ -23,49 +59,13 @@ export default class SearchFiltersComponent extends Component {
     set(this.query, fieldName, value);
   }
 
-  @action
-  submit(event) {
-    event.preventDefault();
-    // If the validation returns no error then we can emit the search event.
-    if (!(this.extendedSearch && this.validateSearchQuery(this.query))) {
-      this.args.search.perform(this.query);
+  @task *submit() {
+    debugger;
+    if (this.changeset.changes.length === 0) {
+      this.validationErrors = [this.intl.t("ember-gwr.search.minFilterError")];
+      yield;
+    } else {
+      this.args.search.perform(this.changeset.change);
     }
-  }
-
-  // This is ugly state modifying code with side effects. But i didnt know a nicer way.
-  validateSearchQuery({ realestateIdentification, modifyDate, createDate }) {
-    this.validationErrors = {};
-    // Since the api needs a number for the realestateIdentification
-    // We have to check that the number is set.
-    if (
-      (realestateIdentification.EGRID ||
-        realestateIdentification.subDistrict) &&
-      !realestateIdentification.number
-    ) {
-      this.validationErrors.realestateIdentification = this.intl.t(
-        "ember-gwr.components.search.validation.realestateIdentification"
-      );
-    }
-
-    // If setting a date filter, both the to and from date have to be set.
-    if (
-      (modifyDate.dateTo || modifyDate.dateFrom) &&
-      !(modifyDate.dateTo && modifyDate.dateFrom)
-    ) {
-      this.validationErrors.modifyDate = this.intl.t(
-        "ember-gwr.components.search.validation.bothDatesSet"
-      );
-    }
-
-    if (
-      (createDate.dateTo || createDate.dateFrom) &&
-      !(createDate.dateTo && createDate.dateFrom)
-    ) {
-      this.validationErrors.createDate = this.intl.t(
-        "ember-gwr.components.search.validation.bothDatesSet"
-      );
-    }
-    // Return a boolean if there are errors or not
-    return Object.keys(this.validationErrors).length;
   }
 }
