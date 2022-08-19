b02f7a2fcdf4620cde23920e372ca6f950a21ebb
Stephan Hug <stephan.hug@adfinis.com>
2022-08-19 09:49:28 +0200

    chore: replace ProjectForm::Field usage in link building modal

--- addon/components/link-building-modal.js
+++ addon/components/link-building-modal.js
@@ -3,16 +3,24 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import BuildingWork from "ember-ebau-gwr/models/building-work";
 
+// Remove the state "Neubau" from the array since you cant link
+// existing buildings as "Neubau".
+const KIND_OF_WORK_OPTIONS = BuildingWork.kindOfWorkOptions.slice(1);
+
 export default class LinkBuildingModalComponent extends Component {
   @service config;
+  @service intl;
   @tracked buildingWork = new BuildingWork();
-  // Remove the state "Neubau" from the array since you cant link
-  // existing buildings as "Neubau".
-  kindOfWorkOptions = BuildingWork.kindOfWorkOptions.slice(1);
 
   constructor(...args) {
     super(...args);
-    this.buildingWork = new BuildingWork();
-    this.buildingWork.kindOfWork = this.kindOfWorkOptions[0];
+    this.buildingWork.kindOfWork = this.kindOfWorkOptions[0].value;
+  }
+
+  get kindOfWorkOptions() {
+    return KIND_OF_WORK_OPTIONS.map((option) => ({
+      value: option,
+      label: this.intl.t(`ember-gwr.buildingWork.kindOfWorkOptions.${option}`),
+    }));
   }
 }
