db5414a6f24c358534c46da34c1438851b22ce75
Stephan Hug <stephan.hug@adfinis.com>
2022-08-19 16:34:17 +0200

    chore: remove project-form

--- addon/components/project-form/checkbox.js
+++
@@ -1,9 +0,0 @@
-import { action } from "@ember/object";
-import Component from "@glimmer/component";
-
-export default class ProjectFormCheckboxComponent extends Component {
-  @action
-  update(event) {
-    this.args.update(event.target.checked);
-  }
-}
