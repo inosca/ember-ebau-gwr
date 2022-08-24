758fb2b17620af1ec44e0c29e0de3eb14bcee13d
Jonas Cosandey <jonas.cosandey@adfinis.com>
2022-08-24 16:22:59 +0200

    style(*): prettier

--- tests/integration/components/search/search-test.js
+++ tests/integration/components/search/search-test.js
@@ -1,24 +1,24 @@
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setupRenderingTest } from "ember-qunit";
-import { module, test } from "qunit";
+import { module, todo } from "qunit";
 
 module("Integration | Component | Search", function (hooks) {
   setupRenderingTest(hooks);
 
-  // test("it renders FilterInputs", async function (assert) {
-  //   await render(hbs`<Search>`);
+  todo("it renders FilterInputs", async function (assert) {
+    await render(hbs`<Search/>`);
 
-  //   assert.equal(this.element.textContent.trim(), "");
+    assert.equal(this.element.textContent.trim(), "");
 
-  //   // Template block usage:
-  //   await render(hbs`
-  //     <Search::ResultTable>
-  //       template block text
-  //     </Search::ResultTable>
-  //   `);
+    // Template block usage:
+    await render(hbs`
+      <Search::ResultTable>
+        template block text
+      </Search::ResultTable>
+    `);
 
-  //   assert.equal(this.element.textContent.trim(), "template block text");
-  // });
-  // test("calls the supplied service search function with the correct arguments");
+    assert.equal(this.element.textContent.trim(), "template block text");
+  });
+  // todo("calls the supplied service search function with the correct arguments");
 });
