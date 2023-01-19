0319233bda73d282ffb9b11f166fba3344e2e17e
Jonas Cosandey <jonas.cosandey@adfinis.com>
2023-01-19 12:36:05 +0100

    fix(maintenance-costs): only display maintenance-costs link in q1

--- addon/helpers/current-quarter.js
+++ addon/helpers/current-quarter.js
@@ -3,32 +3,33 @@ import { DateTime } from "luxon";
 
 // The quarterly closure starts two weeks before the actual quarter finishes.
 const quarters = [
-  { text: "Q4", firstWeekOfQuarter: 50 },
-  { text: "Q3", firstWeekOfQuarter: 37 },
-  { text: "Q2", firstWeekOfQuarter: 24 },
-  { text: "Q1", firstWeekOfQuarter: 11 },
-  { text: "Q4", firstWeekOfQuarter: 1 },
+  { quarter: 4, label: "Q4", firstWeekOfQuarter: 50 },
+  { quarter: 3, label: "Q3", firstWeekOfQuarter: 37 },
+  { quarter: 2, label: "Q2", firstWeekOfQuarter: 24 },
+  { quarter: 1, label: "Q1", firstWeekOfQuarter: 11 },
+  { quarter: 4, label: "Q4", firstWeekOfQuarter: 1 },
 ];
 
-export const getCurrentQuarter = (_, { showYear = false }) => {
-  const now = DateTime.now();
-  const currentWeek = now.toFormat("W");
+export const getCurrentQuarter = (date = DateTime.now()) => {
+  const currentWeek = parseInt(date.toFormat("W"));
 
-  const quarter = quarters.find(
+  const currentQuarter = quarters.find(
     ({ firstWeekOfQuarter }) => currentWeek >= firstWeekOfQuarter
-  )?.text;
-
-  if (showYear) {
+  );
+  return {
+    ...currentQuarter,
     // If we're already in the new year but not yet in week 11, this means we're still
     // working on the Q4 closure from last year. That's why we subtract one year from today.
-    const year = (
-      quarter === "Q4" && currentWeek < 11 ? now.minus({ years: 1 }) : now
-    ).toFormat("yyyy");
-
-    return `${quarter}/${year}`;
-  }
-
-  return quarter;
+    year: (currentQuarter.quarter === 4 && currentWeek < 11
+      ? date.minus({ years: 1 })
+      : date
+    ).toFormat("yyyy"),
+  };
 };
 
-export default helper(getCurrentQuarter);
+export const getCurrentQuarterString = (_, { showYear = false }) => {
+  const { label, year } = getCurrentQuarter();
+  return showYear ? `${label}/${year}` : label;
+};
+
+export default helper(getCurrentQuarterString);
