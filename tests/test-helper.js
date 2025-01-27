e2514107041ab30629634818d354dcb416a2aeee
Jonas Cosandey <jonas.cosandey@adfinis.com>
2025-01-27 14:07:06 +0100

    chore(deps): major updates

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,6 @@
 import { setApplication } from "@ember/test-helpers";
-import { start } from "ember-qunit";
+import { start, setupEmberOnerrorValidation } from "ember-qunit";
+import { loadTests } from "ember-qunit/test-loader";
 import * as QUnit from "qunit";
 import { setup } from "qunit-dom";
 
@@ -10,4 +11,6 @@ setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 
+setupEmberOnerrorValidation();
+loadTests();
 start();
